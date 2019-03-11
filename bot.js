235db89f6651c7cba14ae144477410de672d983e
Sr4B702 <48265543+Sr4B702@users.noreply.github.com>
2019-03-12 00:28:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-﻿const Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const myid = ['552647908478943243'];
@@ -47,7 +47,7 @@ if (message.content === prefix+'spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**- ابجد هوز حطي كلمن سعفس كرشط برشط هرشطط **[ " ${x} " ]`)
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -67,13 +67,25 @@ client.on('message', message => {
 
 
 
-if (command == "تحدث") {
-let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
-if (!rank) return message.reply('#credit @Omar Alzhrani#7191 1000 ')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
+client.on("message",async msg => {
+    if(msg.content.startsWith(prefix + "say")){
+        let args = '';
+        let fltr = m => m.author.id === msg.author.id
+        await msg.reply("**اكتب شي لجعلي اكتب شيئ الان**").then(p =>{
+            p.delete(2222)
+        })
+        msg.channel.awaitMessages(fltr, {
+time: 60000,
+max: 1
+        })
+        .then(sy => {
+            args = sy.first().content
+            sy.first().delete();
+            msg.delete(2222);
+            msg.channel.send(args)
+        })
+    }
+})
 
 client2.on('message', message => {
     if(message.content === prefix+'راتب'){
@@ -98,7 +110,7 @@ if (message.content === prefix+'spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`** - آبجد هوز حطي كلمن صعفز كرشط برشط هرشط **[ " ${x} " ]`)
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -107,24 +119,26 @@ if (message.content === prefix+'spam') {
       }
 });
 
-client2.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-
-if (command == "تحدث") {
-let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
-if (!rank) return message.reply(' ')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
+client2.on("message",async msg => {
+    if(msg.content.startsWith(prefix + "say")){
+        if(msg.author.id !== '552647908478943243') return;
+        let args = '';
+        let fltr = m => m.author.id === msg.author.id
+        await msg.reply("**اكتب شي لجعلي اكتب شيئ الان**").then(p =>{
+            p.delete(2222)
+        })
+        msg.channel.awaitMessages(fltr, {
+time: 60000,
+max: 1
+        })
+        .then(sy => {
+            args = sy.first().content
+            sy.first().delete();
+            msg.delete(2222);
+            msg.channel.send(args)
+        })
+    }
+})
 
 
 client.login(process.env.TOKEN);
